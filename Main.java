117c9dab2588516affb288bcbeda8d15b148a98c
nikicus <35547646+nikicus@users.noreply.github.com>
2018-01-17 19:56:32 -0600

    First program in Coder Girl

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi! I like tea!");
+        System.out.println("okay");
+        System.out.println("Niki");
+    }
+
+}
