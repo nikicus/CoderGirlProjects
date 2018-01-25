69e73e31f496089cd81dc0e7e6a9d30a85cdf979
nikicus <35547646+nikicus@users.noreply.github.com>
2018-01-24 18:22:56 -0600

    Add files via upload

---
+++ GroceryList.java
@@ -0,0 +1,36 @@
+import static java.lang.System.out;
+public class GroceryList
+        {
+            public static void main(String[] args)
+            {
+                System.out.println("milk!");
+
+                System.out.println("Please list three items on your grocery shopping list.");
+
+                System.out.println("Item 1? milk!");
+                System.out.println("Item 2? eggs!");
+                System.out.println("Item 3? bacon!");
+
+                System.out.println("Now, please enter; the quantity; of each item.");
+
+                System.out.println("How many milk?");
+                System.out.println("How many eggs?");
+                System.out.println("How many bacon?");
+
+                System.out.println("Now;, please enter; the price; of each; item");
+
+
+                System.out.println("How much does one milk cost? 2.59");
+                System.out.println("How much does 2 eggs cost? .50");
+                System.out.println("How much does bacon cost? 2.59");
+                System.out.println("Calculating your grocery bill.");
+                System.out.println("Your total cost is");
+
+                System.out.println("13.74");
+
+
+
+        }
+}
+
+
