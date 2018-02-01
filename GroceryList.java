a20e1bc1fc34fb13ae1d8937303779331189f976
nikicus <35547646+nikicus@users.noreply.github.com>
2018-01-31 19:21:11 -0600

    corrected file

--- GroceryList.java
+++ GroceryList.java
@@ -1,32 +1,70 @@
+import java.util.Scanner;
+
 import static java.lang.System.out;
 public class GroceryList
         {
-            public static void main(String[] args)
-            {
-                System.out.println("milk!");
+            public static void main(String[] args) {
+
+                Scanner keyboard = new Scanner(System.in);
+                String item1;
+                String item2;
+                String item3;
+                int quantity1;
+                int quantity2;
+                int quantity3;
+                float item1Cost;
+                float item2Cost;
+                float item3Cost;
+                float totalBill;
+
 
                 System.out.println("Please list three items on your grocery shopping list.");
 
-                System.out.println("Item 1? milk!");
-                System.out.println("Item 2? eggs!");
-                System.out.println("Item 3? bacon!");
+                System.out.println("Item 1?");
+                item1  = keyboard.nextLine();
+
+                System.out.println("Item 2?");
+                item2 = keyboard.nextLine();
+
+                System.out.println("Item 3?");
+                item3 = keyboard.nextLine();
 
                 System.out.println("Now, please enter; the quantity; of each item.");
 
-                System.out.println("How many milk?");
-                System.out.println("How many eggs?");
-                System.out.println("How many bacon?");
+                System.out.println("How many " + item1 +  "?");
+                quantity1 = keyboard.nextInt();
+                keyboard.skip("\n");
+
+                System.out.println("How many " + item2 +  "?");
+                quantity2 = keyboard.nextInt();
+                keyboard.skip("\n");
+
+                System.out.println("How many " + item3 +  "?");
+                quantity3 = keyboard.nextInt();
+                keyboard.skip("\n");
 
                 System.out.println("Now;, please enter; the price; of each; item");
 
 
-                System.out.println("How much does one milk cost? 2.59");
-                System.out.println("How much does 2 eggs cost? .50");
-                System.out.println("How much does bacon cost? 2.59");
-                System.out.println("Calculating your grocery bill.");
-                System.out.println("Your total cost is");
+                System.out.println("How much does one " + item1 + " cost?");
+                item1Cost = keyboard.nextFloat();
+                keyboard.skip("\n");
 
-                System.out.println("13.74");
+                System.out.println("How much does one " + item2 + " cost?");
+                item2Cost = keyboard.nextFloat();
+                keyboard.skip("\n");
+
+                System.out.println("How much does one " + item3 + " cost?");
+                item3Cost = keyboard.nextFloat();
+                keyboard.skip("\n");
+
+                System.out.println("Calculating your grocery bill.");
+                totalBill = (quantity1 * item1Cost) + (quantity2 * item2Cost) + (quantity3 * item3Cost);
+                System.out.println("Your total cost is $" + totalBill);
+
+
+
+
 
 
 
