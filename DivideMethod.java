b9a04200d28396c7273bed54d57df17954921a91
nikicus <35547646+nikicus@users.noreply.github.com>
2018-02-12 10:54:56 -0600

    Add files via upload

---
+++ DivideMethod.java
@@ -0,0 +1,18 @@
+public class DivideMethod {
+
+public static void main (String args[]) {
+    int i = 20;
+    int j = 2;
+
+    System.out.println("i is " + i);
+    System.out.println("j is " + j);
+
+    int k = i / j;
+    System.out.println("i/j is " + k);
+
+    }
+
+
+
+
+}
